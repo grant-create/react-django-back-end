661d4384200843e9f169c4113076fff99eda05d3
grant-create <grant.s.wilkie@gmail.com>
2021-10-15 13:19:55 -0700

    added postgresql

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path, include
-from django.contrib import admin
+
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('movies/', include('movies.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
