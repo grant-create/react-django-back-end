9cbc6fc8bbcd8c74a7fd5855e1774127c1268434
grant-create <grant.s.wilkie@gmail.com>
2021-10-15 14:29:27 -0700

    added db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
+        'NAME': 'testdb',
         'USER': 'wilkieg',
         'PASSWORD': 'password',
         'HOST': '',
