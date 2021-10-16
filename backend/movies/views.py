f5806f1720056ee0a27969a9341f7d3b9cb5db22
grant-create <grant.s.wilkie@gmail.com>
2021-10-15 18:36:50 -0700

    updated for now

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from movies.config import api_key
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("hello, you're in the movie index")
+
+def home(request):
+    response = request.get(f'http://www.omdbapi.com/?i=tt3896198&apikey={api_key}')
+    moviedata = response.json()
+    return render(request)
